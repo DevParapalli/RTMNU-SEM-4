8b1c24f25ccdc310ee56532af7c0995b0b51cda0
Devansh Parapalli <parapallidev@gmail.com>
2023-05-03 13:01:22 +0530

    edited shadowing in CW-II 7

--- CW-II/Practical_08_list_to_unique/main.py
+++ CW-II/Practical_08_list_to_unique/main.py
@@ -2,8 +2,8 @@
 
 LIST = [1, 1, 1, 2, 2, 3, 3, 4]
 
-def get_unique_list(list):
-    return list(set(list))
+def get_unique_list(_l):
+    return list(set(_l))
 
 if __name__ == "__main__":
     print(get_unique_list(LIST))
