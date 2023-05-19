a56e6175f632657703b4c81ec798d3cf27f82ea7
Devansh Parapalli <parapallidev@gmail.com>
2023-05-19 15:08:21 +0530

    Finished Practical 09

--- DBMS/Practical_09/main.py
+++ DBMS/Practical_09/main.py
@@ -1,6 +1,5 @@
-from json import dumps
 from copy import deepcopy
-from re import findall, sub
+from re import sub
 
 O_REL = ['A', 'B', 'C', 'D']
 FD = [
@@ -14,7 +13,7 @@ NEW_REL = [
     ['C', 'D'],
 ]
 
-def fuzzy_count(_itr: list[str], elem: str):
+def in_count(_itr: list[str], elem: str):
     return sum([1 for i in _itr if elem in i])
 
 def initialize(old_rel, new_rel):
@@ -34,34 +33,74 @@ def attrs_intersection(attrs: list[str]) -> str:
         s = s.replace(" ", "")
     return 'a' if all([True if 'a' in attr else False for attr in attrs]) else 'b'
 
+def get_lhs_values(dep, table, old_rel):
+    check_lhs: list[str] = []
+    compound_attr: list[list[str]] = []
+    for attr in dep[0]:
+        compound_attr.append([row[old_rel.index(attr)] for row in table])
+    for i, c_attr in enumerate(map(list, zip(*compound_attr))):
+        check_lhs.append(attrs_intersection(c_attr))
+        # at this point we have a comparison list, that we can use to check for the step 2 conditions
+    return check_lhs
+
+def print_table(table: list[list[str]]):
+    pretty_table = ""
+    for row in table:
+        pretty_table += "|"
+        for columns in row:
+            pretty_table += f"{columns}\t|"
+        pretty_table += "\n"
+    
+    length = len(pretty_table.split("\n")[0].expandtabs())
+    print("_" * length)
+    print(pretty_table)
+    
 def woolman_algorithm_test(old_rel: list[str], new_rel: list[list[str]], fd: list[list[str]]) -> bool:
     # Step 1: Initialize
     table = initialize(old_rel, new_rel)
-
+    print("Initial Table: ")
+    print_table(table)
     while True:
         compare_table = deepcopy(table)
         for dep in fd:
-            check_lhs: list[str] = []
-            compound_attr: list[list[str]] = []
-            for attr in dep[0]:
-                compound_attr.append([row[old_rel.index(attr)] for row in table])
-            for i, c_attr in enumerate(map(list, zip(*compound_attr))):
-                check_lhs.append(attrs_intersection(c_attr))
-                # at this point we have a comparison list, that we can use to check for the step 2 conditions
-                pass # breakpoint
+            print(f"Checking FD: {dep[0]} -> {dep[1]}")
+            check_lhs = get_lhs_values(dep, table, old_rel)
             
-            # Check if RHS is multipart
-            UPDATION_LIST: list[list[str]] = []
-            for attr in dep[1]:
-                UPDATION_LIST.append([row[old_rel.index(attr)] for row in table])
-
-        print(dumps(table, indent=4))
+            rows_to_change = [i for i, x in enumerate(check_lhs) if x == "a"]
+            columns_to_change = [old_rel.index(attr) for attr in dep[1]]
+            column_values = ["X" for _ in columns_to_change]
+            
+            if check_lhs.count("a") <= 1: 
+                print("No common attributes found")
+                continue
+            
+            for column_index in columns_to_change:
+                test_column = [row[column_index] for row in table if table.index(row) in rows_to_change]
+                # Find if there is "A" in the columns we wish to update, else get the first "B" value
+                if in_count(test_column, "a") >= 1:
+                    column_values[columns_to_change.index(column_index)] = f"a({column_index})"
+                else:
+                    column_values[columns_to_change.index(column_index)] = test_column[0]
+            
+            # Do da update
+            for row_index, row in enumerate(table):
+                for col_index, col in enumerate(row):
+                    if col_index in columns_to_change and row_index in rows_to_change:
+                        table[row_index][col_index] = column_values[columns_to_change.index(col_index)]
+            # Print Table
+            print_table(table)
+        
         if compare_table == table:
             for row in table:
-                if fuzzy_count(row, "a") == len(row):
+                if in_count(row, "a") == len(row):
+                    print(f"Found a row with all 'a' values. Row: {table.index(row) + 1}")
                     return True
+            print("No more changes possible.")
             return False
     
     
 if __name__ == '__main__':
-    woolman_algorithm_test(O_REL, NEW_REL, FD)
+    if woolman_algorithm_test(O_REL, NEW_REL, FD):
+        print("The given relation decomposition is NOT LOSSY.")
+    else:
+        print("The given relation decomposition is LOSSY.")
